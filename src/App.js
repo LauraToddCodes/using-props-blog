8bb38e68d35959a4b1d1a962b97c915411189c87
LauraToddCode <laura20todd@gmail.com>
2021-03-08 14:19:21 +0000

    Completed code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Selection from "./Selection";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ value, setValue ] = React.useState();
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    return (
+        <React.Fragment>
+            <form onChange={handleChange}>
+                <h2>What's the best name for a dog?</h2>
+                <div>
+                    <input type="radio" id="graham" value="Graham" name="name"/>
+                    <label for="Graham">Graham</label>
+                </div>
+                <div>
+                    <input type="radio" id="kenneth" value="Kenneth" name="name"/>
+                    <label for="Kenneth">Kenneth</label>
+                </div>
+                <div>
+                    <input type="radio" id="steve" value="Alan" name="name"/>
+                    <label for="Alan">Alan</label>
+                </div>
+            </form>
+            <Selection selection={value}/>
+        </React.Fragment>
+    )
 }
 
 export default App;
---
+++ src/Selection.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Selection(props) {
+    return (
+        <h3>{props.selection}</h3>
+    )
+}
+
+export default Selection;
